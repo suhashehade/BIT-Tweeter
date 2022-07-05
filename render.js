81a9c2deca1851d6040ad1fc6e08f9168988e014
suha <suhashehahde@gmail.com>
2022-07-05 10:45:17 -0700

    add tweeter project

--- render.js
+++ render.js
@@ -3,14 +3,15 @@ const Renderer = function () {
     $("#posts").html("");
     for (let post of posts) {
       $("#posts").append(
-        `<div class="post" data-id="${post.id}">${post.text}</div>`
+        `<div class="post" id="${post.id}" data-id="${post.id}">${post.text}</div>`
       );
       let index = post.id.charAt(1) - 1;
+      let post_id = "#" + post.id;
       for (let comment of posts[index].comments) {
-        $(".post").append(
+        $(post_id).append(
           `<div class="comments">
                <div class="comment" data-id="${comment.id}">${comment.text}</div>
-            </div>`
+           </div>`
         );
       }
     }
